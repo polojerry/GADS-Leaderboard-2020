5901c1397e0a97de347751900f72838e3d234108
polojerry <polojerry4@gmail.com>
2020-09-09 12:12:56 +0300

    SkillIQ Leaders and Learners Leaders Design Done

---
+++ app/src/main/java/com/polotechnologies/leaderboard/dataModel/SkillIqLeader.kt
@@ -0,0 +1,8 @@
+package com.polotechnologies.leaderboard.dataModel
+
+data class SkillIqLeader (
+    val name  :String,
+    val hours  :Int,
+    val country  :String,
+    val badgeUrl  :String,
+)
